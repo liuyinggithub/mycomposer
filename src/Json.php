3d0578001cecfb59e9878fee7e64c58c55b91b70
liuying <liuyinggithub@users.noreply.github.com>
2016-10-10 11:00:06 +0800

    Update Json.php

--- src/Json.php
+++ src/Json.php
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-namespace mycomposer;
+namespace mycomposer01;
 class Json
 {
     public static function encode($data)
